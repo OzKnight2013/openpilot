1751fb6836f61dacd0b819fdb5755feff3f11dbd
AskAlice <alice@askalice.me>
2020-05-20 17:25:25 -0600

    panda

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -136,13 +136,13 @@ static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
 
     // exit controls on rising edge of gas press
-    if (addr == 608) {
-      bool gas_pressed = (GET_BYTE(to_push, 7) >> 6) != 0;
-      if (!unsafe_allow_gas && gas_pressed && !gas_pressed_prev) {
-        controls_allowed = 0;
-      }
-      gas_pressed_prev = gas_pressed;
-    }
+    // if (addr == 608) {
+    //   bool gas_pressed = (GET_BYTE(to_push, 7) >> 6) != 0;
+    //   if (!unsafe_allow_gas && gas_pressed && !gas_pressed_prev) {
+    //     controls_allowed = 0;
+    //   }
+    //   gas_pressed_prev = gas_pressed;
+    // }
 
     // sample subaru wheel speed, averaging opposite corners
     if (addr == 902) {
@@ -153,13 +153,15 @@ static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
 
     // exit controls on rising edge of brake press
-    if (addr == 916) {
-      bool brake_pressed = (GET_BYTE(to_push, 6) >> 7) != 0;
-      if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
-        controls_allowed = 0;
-      }
-      brake_pressed_prev = brake_pressed;
-    }
+    // I'm not advertising this repository anywhere, and this vehicle does NOT have long control working,
+    // and considering comma said that they only enforce this on honda and toyota at the moment, I feel nothing wrong with doing something similar to xx979xx
+    // if (addr == 916) {
+    //   bool brake_pressed = (GET_BYTE(to_push, 6) >> 7) != 0;
+    //   if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
+    //     controls_allowed = 0;
+    //   }
+    //   brake_pressed_prev = brake_pressed;
+    // }
 
     // check if stock camera ECU is on bus 0
     if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && (addr == 832)) {
